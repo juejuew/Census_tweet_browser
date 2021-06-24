b50fa851b0ebd4821b5e2626199ac688701332bf
juejuew <45111420+juejuew@users.noreply.github.com>
2021-06-24 11:23:23 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker run -it --name twibrowser_update_container -p 127.0.0.1:80:80 -p 127.0.0.1:8050:8050 juejue64807761/twibrowser_update
-# docker rm twibrowser_update_container
+docker run -it --name twibrowser_fromgit_container -p 127.0.0.1:80:80 -p 127.0.0.1:8050:8050 juejue64807761/twibrowser_fromgit
+# docker rm twibrowser_fromgit_container
