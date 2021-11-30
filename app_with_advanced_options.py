ce880b10ca5ca717d5d11517e5991a42abaf2eb6
juejuew <45111420+juejuew@users.noreply.github.com>
2021-11-30 15:19:12 -0500

    Update app_with_advanced_options.py

--- app_with_advanced_options.py
+++ app_with_advanced_options.py
@@ -3,10 +3,10 @@
 
 import dash
 import dash_bootstrap_components as dbc
-# import dash_core_components as dcc
-from dash import dcc
-# import dash_html_components as html
-from dash import html
+import dash_core_components as dcc
+# from dash import dcc
+import dash_html_components as html
+# from dash import html
 from dash.dependencies import Input, Output
 # import dash_table
 from dash import dash_table
